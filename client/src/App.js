ab7393ec9a8f479745b44f826935efcf7a290a5c
Jonathan Nuno <jnuno.423@gmail.com>
2020-12-16 10:06:09 -0500

    finalized add, get, delete actions, updated reducer, preparing to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Container } from 'reactstrap';
 
 // Components
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -15,7 +17,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavbar />
+          <Container>
+          <ItemModal />
           <ShoppingList />
+          </Container>
         </div>
       </Provider>
     );
