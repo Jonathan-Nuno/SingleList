a20bf184d70c3ddf89982d3bb3ff4f34f9f10871
Jonathan Nuno <jnuno.423@gmail.com>
2020-12-16 06:35:25 -0500

    implemented redux in react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 // Components
 import AppNavbar from './components/AppNavbar';
@@ -10,10 +12,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
