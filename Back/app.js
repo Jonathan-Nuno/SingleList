740daa3b74362d3e5516f70f0b3389fc72dc94cf
Jonathan Nuno <jnuno.423@gmail.com>
2020-12-18 15:20:19 -0500

    changes for deploy

--- Back/app.js
+++ Back/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
+// const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -18,17 +18,7 @@ mongoose
     .catch(err => console.error(err.message));
 
 // Use Routes
-app.use('/api/items', items)
-
-// // Server static assets if in production
-// if(process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
