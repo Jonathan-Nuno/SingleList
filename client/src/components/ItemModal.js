0177f25637e0261fcb252e5a696bc936018671b0
Jonathan Nuno <jnuno.423@gmail.com>
2020-12-21 21:36:28 -0500

    added loginmodal, updated getitems, delete, and add item actions, updated views for shoppinglist and itemmodal

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,12 +11,17 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 
 class ItemModal extends Component {
     state = {
         modal: false,
         name: ''
+    }
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool
     }
 
     toggle = () => {
@@ -26,7 +31,7 @@ class ItemModal extends Component {
     }
 
     onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value});
+        this.setState({ [e.target.name]: e.target.value });
     };
 
     onSubmit = e => {
@@ -46,17 +51,18 @@ class ItemModal extends Component {
     render() {
         return (
             <div>
-                <Button 
-                color="dark" 
-                style={{ marginBottom: '2rem' }} 
-                onClick={this.toggle}
-                >Add Item
-                </Button>
+                { this.props.isAuthenticated ? (
+                    <Button
+                        color="dark"
+                        style={{ marginBottom: '2rem' }}
+                        onClick={this.toggle}
+                    >Add Item
+                    </Button>
+                ) : (
+                        <h4 className='mb-3 ml-4'>Please log in to manage items</h4>
+                    )}
 
-                <Modal
-                    isOpen={this.state.modal}
-                    toggle={this.toggle}
-                >
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
                     <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
                     <ModalBody>
                         <Form onSubmit={this.onSubmit}>
@@ -83,7 +89,8 @@ class ItemModal extends Component {
 }
 
 const mapStateToProps = state => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStateToProps, { addItem })(ItemModal);
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -6,6 +6,11 @@ import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types'
 
 class ShoppingList extends Component {
+    static propTypes = {
+        getItems: PropTypes.func.isRequired,
+        item: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    };
 
     componentDidMount() {
         this.props.getItems();
@@ -24,6 +29,7 @@ class ShoppingList extends Component {
                         {items.map(({ _id, name }) => (
                             <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
+                                    { this.props.isAuthenticated ? (
                                     <Button
                                         className="remove-btn"
                                         color="danger"
@@ -33,6 +39,7 @@ class ShoppingList extends Component {
                                     >
                                         &times;
                                     </Button>
+                                    ) : null}
                                     {name}
                                 </ListGroupItem>
                             </CSSTransition>
@@ -44,13 +51,9 @@ class ShoppingList extends Component {
     }
 }
 
-ShoppingList.propTypes = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired
-}
-
 const mapStateToProps = (state) => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
